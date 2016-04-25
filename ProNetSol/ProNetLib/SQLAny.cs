f34351ffe5041bb8479bea33b902c98ad5ba3744
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2016-04-25 18:40:56 +0200

    Empresa

--- ProNetSol/ProNetLib/SQLAny.cs
+++ ProNetSol/ProNetLib/SQLAny.cs
@@ -26,6 +26,9 @@ namespace ProNetLib
                 case "empresas":
                     return await Empresas(v[1]);
                     break;
+                case "empresa":
+                    return await Empresa(v[1], v[2]);
+                    break;
                 default:
                     return String.Format("ERROR: Unknow command [{0}]", input);
                     break;
@@ -94,6 +97,60 @@ namespace ProNetLib
         public async Task<string> Empresas(string dsn)
         {
             string queryString = "SELECT * FROM empresas";
+
+            IList<Empresa> empresas = new List<Empresa>();
+            using (OdbcConnection connection = new OdbcConnection("DSN=" + dsn))
+            {
+                OdbcCommand command = new OdbcCommand(queryString, connection);
+
+                connection.Open();
+
+                // Execute the DataReader and access the data.
+                OdbcDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Empresa e = new Empresa();
+                    e.proId = (string)reader["codigo"];
+                    e.nombre = (string)reader["nombre"];
+                    if (!reader.IsDBNull(2))
+                        e.nif = (string)reader["nif"];
+                    if (!reader.IsDBNull(4))
+                        e.fechaAlta = (DateTime)reader["fecha_alta"];
+                    if (!reader.IsDBNull(5))
+                        e.fechaBaja = (DateTime?)reader["fecha_baja"];
+                    e.activa = true;
+                    if ((string)reader["activo"] == "N")
+                        e.activa = false;
+                    if (!reader.IsDBNull(6))
+                        e.contacto1 = (string)reader["contacto1"];
+                    if (!reader.IsDBNull(7))
+                        e.contacto2 = (string)reader["contacto2"];
+                    if (!reader.IsDBNull(8))
+                        e.direccion = (string)reader["direccion"];
+                    if (!reader.IsDBNull(11))
+                        e.codPostal = (string)reader["distrito"];
+                    if (!reader.IsDBNull(9))
+                        e.poblacion = (string)reader["poblacion"];
+                    if (!reader.IsDBNull(10))
+                        e.provincia = (string)reader["provincia"];
+                    if (!reader.IsDBNull(13))
+                        e.fax = (string)reader["fax"];
+                    if (!reader.IsDBNull(14))
+                        e.email = (string)reader["email"];
+                    if (!reader.IsDBNull(20))
+                        e.observaciones = (string)reader["notas"];
+                    empresas.Add(e);
+                }
+
+                // Call Close when done reading.
+                reader.Close();
+            }
+            return JsonConvert.SerializeObject(empresas); ;
+        }
+
+        public async Task<string> Empresa(string dsn, string codigo)
+        {
+            string queryString = "SELECT * FROM empresas WHERE codigo = " + codigo;
 
             IList<Empresa> empresas = new List<Empresa>();
             using (OdbcConnection connection = new OdbcConnection("DSN=" + dsn))
